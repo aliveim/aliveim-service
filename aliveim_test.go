ee5fd7e8e2e6f936be71bf329c9a1ea1d10e3c4e
Andrea Grandi <a.grandi@gmail.com>
2015-03-13 16:27:41 +0000

    Add test method for parseAlivePost function

---
+++ aliveim_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"testing"
+)
+
+type nopCloser struct {
+	io.Reader
+}
+
+func (nopCloser) Close() error { return nil }
+
+func TestParseAlivePost(t *testing.T) {
+	var body io.ReadCloser = nopCloser{bytes.NewBufferString(`{"device_id": "abc123", "timeout": 300}`)}
+	var ar AliveRequest = parseAlivePost(body)
+
+	if ar.DeviceID != "abc123" || ar.Timeout != 300 {
+		t.Fatalf("Expected: DeviceID: %s, Timeout: %d, got DeviceID: %s, Timeout: %d", "abc123", 300, ar.DeviceID, ar.Timeout)
+	}
+}
